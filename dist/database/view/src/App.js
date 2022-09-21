1a2bb8530a7beb36380cc376d2bab6a0d264b69f
christopher nafrere <nafrere.christopher@gmail.com>
2022-09-21 21:46:22 +0200

    organise as project

---
+++ dist/database/view/src/App.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./App.css");
+function App() {
+    return (<div className="App">
+      <h1>Nouveau Site</h1>
+    </div>);
+}
+exports.default = App;
